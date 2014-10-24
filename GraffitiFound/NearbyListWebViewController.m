da4fcc8dcde7d8765900aadf5a1b25e1fff054cd
RememberLenny <leonard_bogdonoff@condenast.com>
2014-10-24 16:40:03 -0400

    Create portrait mode

--- GraffitiFound/NearbyListWebViewController.m
+++ GraffitiFound/NearbyListWebViewController.m
@@ -26,4 +26,27 @@
     }
 }
 
+- (void)splitViewController:(UISplitViewController *)svc
+     willHideViewController:(UIViewController *)aViewController
+          withBarButtonItem:(UIBarButtonItem *)barButtonItem
+       forPopoverController:(UIPopoverController *)pc
+{
+    // If this bar button item does not have a title, it will not appear at all
+    barButtonItem.title = @"Nearby Graffiti";
+    
+    // Take this bar button item and put it on the left side of the nav item
+    self.navigationItem.leftBarButtonItem = barButtonItem;
+}
+
+- (void)splitViewController:(UISplitViewController *)svc
+     willShowViewController:(UIViewController *)aViewController
+  invalidatingBarButtonItem:(UIBarButtonItem *)barButtonItem
+{
+    // Remove the bar button item from the navigation item
+    // Double check that it is the correct button, even though we know it is
+    if (barButtonItem == self.navigationItem.leftBarButtonItem) {
+        self.navigationItem.leftBarButtonItem = nil;
+    }
+}
+
 @end
