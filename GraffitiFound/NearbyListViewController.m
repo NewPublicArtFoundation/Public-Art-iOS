416fe655aa45809278a6d96435a379dc2b9784e3
RememberLenny <leonard_bogdonoff@condenast.com>
2014-10-24 15:02:25 -0400

    Setup uitableviewcontroller + uikit

---
+++ GraffitiFound/NearbyListViewController.m
@@ -0,0 +1,13 @@
+//
+//  NearbyListViewController.m
+//  GraffitiFound
+//
+//  Created by Leonard Bogdonoff on 10/24/14.
+//  Copyright (c) 2014 New Public Art Foundation. All rights reserved.
+//
+
+#import "NearbyListViewController.h"
+
+@implementation NearbyListViewController
+
+@end
