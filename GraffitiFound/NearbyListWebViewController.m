edf43636a125ce6aab71483472c59639c0a45c7f
RememberLenny <leonard_bogdonoff@condenast.com>
2014-10-24 16:02:22 -0400

    Set up the loadview to run webview and set the webview URL with the url property

--- GraffitiFound/NearbyListWebViewController.m
+++ GraffitiFound/NearbyListWebViewController.m
@@ -10,4 +10,20 @@
 
 @implementation NearbyListWebViewController
 
+- (void)loadView
+{
+    UIWebView *webView = [[UIWebView alloc] init];
+    webView.scalesPageToFit = YES;
+    self.view = webView;
+}
+
+- (void)setURL:(NSURL *)URL
+{
+    _URL = URL;
+    if (_URL) {
+        NSURLRequest *req = [NSURLRequest requestWithURL:_URL];
+        [(UIWebView *)self.view loadRequest:req];
+    }
+}
+
 @end
