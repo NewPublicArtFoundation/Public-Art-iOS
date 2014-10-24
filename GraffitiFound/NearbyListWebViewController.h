749b50a125cad3e9e56a0eead6f8ea278136e9b8
RememberLenny <leonard_bogdonoff@condenast.com>
2014-10-24 15:59:56 -0400

    Create a webview detail controller

---
+++ GraffitiFound/NearbyListWebViewController.h
@@ -0,0 +1,15 @@
+//
+//  NearbyListWebViewController.h
+//  GraffitiFound
+//
+//  Created by Leonard Bogdonoff on 10/24/14.
+//  Copyright (c) 2014 New Public Art Foundation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
+
+@interface NearbyListWebViewController : UIViewController
+
+
+@end
