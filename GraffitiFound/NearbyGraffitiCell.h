ede023fe3bcc0c151ee03d82ef351190eac4192d
RememberLenny <leonard_bogdonoff@condenast.com>
2014-10-24 16:42:00 -0400

    Create the cell for nearby graffiti

---
+++ GraffitiFound/NearbyGraffitiCell.h
@@ -0,0 +1,14 @@
+//
+//  NearbyGraffitiCell.h
+//  GraffitiFound
+//
+//  Created by Leonard Bogdonoff on 10/24/14.
+//  Copyright (c) 2014 New Public Art Foundation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
+
+@interface NearbyGraffitiCell : UITableViewCell
+
+@end
---
+++ GraffitiFound/NearbyGraffitiCell.m
@@ -0,0 +1,13 @@
+//
+//  NearbyGraffitiCell.m
+//  GraffitiFound
+//
+//  Created by Leonard Bogdonoff on 10/24/14.
+//  Copyright (c) 2014 New Public Art Foundation. All rights reserved.
+//
+
+#import "NearbyGraffitiCell.h"
+
+@implementation NearbyGraffitiCell
+
+@end
