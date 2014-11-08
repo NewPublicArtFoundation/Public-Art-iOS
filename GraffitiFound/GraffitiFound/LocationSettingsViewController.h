aa8bf22963c3467801944aa60387af81cade29b4
RememberLenny <leonard_bogdonoff@condenast.com>
2014-11-08 16:39:18 -0500

    Adding the segmented view chang

--- GraffitiFound/GraffitiFound/LocationSettingsViewController.h
+++ GraffitiFound/GraffitiFound/LocationSettingsViewController.h
@@ -8,6 +8,7 @@
 
 #import <UIKit/UIKit.h>
 #import <Foundation/Foundation.h>
+#import "INTULocationManager.h"
 
 @interface LocationSettingsViewController : UIViewController
 
--- GraffitiFound/GraffitiFound/LocationSettingsViewController.m
+++ GraffitiFound/GraffitiFound/LocationSettingsViewController.m
@@ -10,6 +10,7 @@
 
 @interface LocationSettingsViewController ()
 
+@property (assign, nonatomic) INTULocationAccuracy desiredAccuracy;
 @property (weak, nonatomic) IBOutlet UILabel *curLocationLabel;
 @property (weak, nonatomic) IBOutlet UILabel *locationAccuLabel;
 @property (weak, nonatomic) IBOutlet UITextField *curLocationTextInput;
@@ -23,9 +24,29 @@
 
 @implementation LocationSettingsViewController
 
+- (IBAction)desiredAccuracyControlChanged:(UISegmentedControl *)sender
+{
+    switch (sender.selectedSegmentIndex) {
+        case 0:
+            self.locationAccuControl = INTULocationAccuracyBlock;
+            break;
+        case 1:
+            self.locationAccuControl = INTULocationAccuracyNeighborhood;
+            break;
+        case 2:
+            self.locationAccuControl = INTULocationAccuracyCity;
+            break;
+        default:
+            break;
+    }
+}
+
 -(void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    self.desiredAccuracyControl.selectedSegmentIndex = 0;
+    self.desiredAccuracy = INTULocationAccuracyCity;
 }
 
 -(instancetype)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil{
