a0a01285a9812b6f465313d9cafa8cb9f539543f
RememberLenny <leonard_bogdonoff@condenast.com>
2014-11-08 13:48:10 -0500

    Save the new location settings view

---
+++ GraffitiFound/GraffitiFound/LocationSettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  LocationSettingsViewController.h
+//  graffitifound
+//
+//  Created by Leonard Bogdonoff on 11/8/14.
+//  Copyright (c) 2014 New Public Art Foundation. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LocationSettingsViewController : NSObject
+
+@end
---
+++ GraffitiFound/GraffitiFound/LocationSettingsViewController.m
@@ -0,0 +1,13 @@
+//
+//  LocationSettingsViewController.m
+//  graffitifound
+//
+//  Created by Leonard Bogdonoff on 11/8/14.
+//  Copyright (c) 2014 New Public Art Foundation. All rights reserved.
+//
+
+#import "LocationSettingsViewController.h"
+
+@implementation LocationSettingsViewController
+
+@end
