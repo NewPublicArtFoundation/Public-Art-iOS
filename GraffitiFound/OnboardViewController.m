b2080b67d468f18c496ddbbce9a70f892873a2e9
RememberLenny <leonard_bogdonoff@condenast.com>
2014-11-20 21:43:12 -0500

    Create the on boarding view controller

---
+++ GraffitiFound/OnboardViewController.m
@@ -0,0 +1,37 @@
+//
+//  OnboardViewController.m
+//  graffitifound
+//
+//  Created by Leonard Bogdonoff on 11/20/14.
+//  Copyright (c) 2014 New Public Art Foundation. All rights reserved.
+//
+
+#import "OnboardViewController.h"
+
+@interface OnboardViewController ()
+
+@end
+
+@implementation OnboardViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
